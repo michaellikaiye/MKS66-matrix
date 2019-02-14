ead58ef48dd689875d64a7295c63229acdc4c4b1
michaellikaiye <mye1@stuy.edu>
2019-02-14 11:09:30 -0500

    hmm

--- matrix.py
+++ matrix.py
@@ -14,20 +14,41 @@ import math
 def print_matrix( matrix ):
     for row in matrix:
         for element in row:
-            print(element,end="")
+            print(element,end=" ")
         print("")
 
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
-    pass
+    size = len(matrix)
+    for i in range(size):
+        for j in range(size):
+            if i is j:
+                matrix[i][j] = 1
+            else:
+                matrix[i][j] = 0
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
-
-
+    #assuming m1 is square, 4x4
+    row = 4
+    col = m2[3].count(1)
+    temp = []
+    for i in range(row):
+        temp.append([])
+        for j in range(col):
+            temp.append(m2[i][j])
+            m2[i][j] = 0
+    
+    print_matrix(m2)
+    print(temp)
+    print("matrix")
+    for i in range(row):
+        for j in range(col):
+            for n in range(row):
+                m2[i][j] += m1[i][n] * temp[n][j]
+    
 
 def new_matrix(rows = 4, cols = 4):
     m = []
