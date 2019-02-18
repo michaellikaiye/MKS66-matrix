5fecb856f444659c5aed293148d390f42e7350e2
michaellikaiye <mye1@stuy.edu>
2019-02-18 15:30:09 -0500

    .

--- main.py
+++ main.py
@@ -5,24 +5,6 @@ from matrix import *
 screen = new_screen()
 color = [200, 200, 200]
 
-art = new_matrix()
-
-f = open("in.txt", 'r')
-lines = f.read().split('\n')
-for line in lines:
-    locs = line.split('/')
-    for i in range(len(locs) - 1):
-        words = locs[i].split(',')
-        start = [int(x) for x in words]
-        words = locs[i + 1].split(',')
-        end = [int(x) for x in words]
-        add_edge(art, start[0] - 100, 500 - start[1],
-                 0, end[0] - 100, 500 - end[1], 0)
-draw_lines(art, screen, color)
-display(screen)
-save_extension(screen, 'img.png')
-f.close()
-
 print("Testing MATRICES")
 print("Testing m2 = new_matrix(4,2)")
 m2 = new_matrix(4, 2)
@@ -45,4 +27,21 @@ print_matrix(m2)
 print("Testing add point (1, 2, 3)")
 add_point(m2, 1, 2, 3)
 print_matrix(m2)
+
+art = new_matrix()
+f = open("in.txt", 'r')
+lines = f.read().split('\n')
+for line in lines:
+    locs = line.split('/')
+    for i in range(len(locs) - 1):
+        words = locs[i].split(',')
+        start = [int(x) for x in words]
+        words = locs[i + 1].split(',')
+        end = [int(x) for x in words]
+        add_edge(art, start[0] - 100, 500 - start[1],
+                 0, end[0] - 100, 500 - end[1], 0)
+f.close()
+draw_lines(art, screen, color)
+display(screen)
+save_extension(screen, 'img.png')
 
